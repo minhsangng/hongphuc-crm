d4b3e668db4c1b65a818b0718ff643cfbf161e48
AzureAD\NGUYENMINHSANG <sangnguyen2@genesolutions.vn>
2026-09-04 12:27:00 +0700

    Update sel

---
+++ src/cron.js
@@ -0,0 +1,23 @@
+const { CronJob } = require("cron");
+const https = require("https");
+
+const job = new CronJob(
+  "*/14 * * * *",
+  () => {
+    https
+      .get("https://mamnonhongphuc.onrender.com", (res) => {
+        if (res.statusCode === 200)
+          console.log("GET request sent successfully");
+        else
+          console.log("GET request failed", res.statusCode);
+      })
+      .on("error", (e) =>
+        console.error("Error while sending request", e)
+      );
+  },
+  null,
+  false,
+  "Asia/Ho_Chi_Minh"
+);
+
+module.exports = job;
